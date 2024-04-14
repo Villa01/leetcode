4b6f04975a1373e3e49f973eb426188b333287d6
Erick Villatoro <3007114090101@ingenieria.usac.edu.gt>
2024-04-14 00:29:51 -0600

    StrStr manual solution

--- problems/find_index_first_occurrence_in_string.go
+++ problems/find_index_first_occurrence_in_string.go
@@ -1,8 +1,24 @@
 package problems
 
-import "strings"
+import (
+	"strings"
+)
 
 func StrStr(haystack string, needle string) int {
 
 	return strings.Index(haystack, needle)
+}
+
+func StrStrManual(haystack string, needle string) int {
+
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+
+		substring := haystack[i : i+len(needle)]
+
+		if strings.Compare(substring, needle) == 0 {
+			return i
+		}
+	}
+
+	return -1
 }
--- tests/find_index_first_occurrence_in_string_test.go
+++ tests/find_index_first_occurrence_in_string_test.go
@@ -26,3 +26,36 @@ func TestStrStr2(t *testing.T) {
 		t.Fatalf(`StrStr(%q", %q) = %q, want %q`, haystack, needle, output, expected)
 	}
 }
+
+func TestStrStrManual(t *testing.T) {
+	haystack := "leetcode"
+	needle := "leeto"
+	expected := -1
+	output := problems.StrStrManual(haystack, needle)
+
+	if output != expected {
+		t.Fatalf(`StrStr(%q", %q) = %q, want %q`, haystack, needle, output, expected)
+	}
+}
+
+func TestStrStrManual2(t *testing.T) {
+	haystack := "sadbutsad"
+	needle := "sad"
+	expected := 0
+	output := problems.StrStrManual(haystack, needle)
+
+	if output != expected {
+		t.Fatalf(`StrStr(%q", %q) = %d, want %d`, haystack, needle, output, expected)
+	}
+}
+
+func TestStrStrManual3(t *testing.T) {
+	haystack := "a"
+	needle := "a"
+	expected := 0
+	output := problems.StrStrManual(haystack, needle)
+
+	if output != expected {
+		t.Fatalf(`StrStr(%q", %q) = %d, want %d`, haystack, needle, output, expected)
+	}
+}
