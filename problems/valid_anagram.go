392c78d7bb62f725c44d94b5b9ff08f1d55a00bb
Erick Villatoro <3007114090101@ingenieria.usac.edu.gt>
2024-04-20 15:40:15 -0600

    Start of valid anagram

---
+++ problems/valid_anagram.go
@@ -0,0 +1,5 @@
+package problems
+
+func isAnagram(s string, t string) bool {
+	return true
+}
---
+++ tests/valid_anagram_test.go
@@ -0,0 +1,9 @@
+package tests
+
+func isAnagramTest(t *testing) {
+	s := "anagram"
+	t := "nagaram"
+
+	r := problems.isAnagram(s, t)
+
+}
